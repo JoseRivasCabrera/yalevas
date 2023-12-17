67372ecab6fc8af574ed910f7994fafcd688aed9
Rivas <jose.trabajo877@gmail.com>
2023-12-17 15:56:44 -0600

    se crea la estructura del archivo db.json y se consumen los datos

--- src/scripts/mostrardatos-index.js
+++ src/scripts/mostrardatos-index.js
@@ -1,30 +1,71 @@
-const insertar = () => {
-    const contenedorArticulos = document.getElementById('contenedor-articulos');
+const crearArticuloIndex = (nombreCategoria, numbVentas, nombre, precioDescuento, precioNeto, descuento, id) => {
+    const articuloIndex = document.createElement('a');
+    articuloIndex.href= '#';
+    articuloIndex.classList.add('caja-articulo')
+    const contenido = `<div class="caja_imagen-articulo" id="caja_imagen-articulo">
+        <img class="imagen-articulo" id="imagen-articulo" src="src/imagenes/img-articulos/mochila.jpg"  alt="imagen de producto">
+       </div>
+      
+       <div class="descripciones">
+         <p class="nombre-txt" >Bella mochila infantil</p>
+        <p class="no-vendidos">16 Vendidos recientemente</p>
+         <p class="precio-txt">¢16.955</p>
+       </div>`
   
-    contenedorArticulos.innerHTML = '';
-  
-    const boxArticulo = `<a class="caja-articulo" href="#">
-  <div class="caja_imagen-articulo" id="caja_imagen-articulo">
-    <img class="imagen-articulo" id="imagen-articulo" src="src/imagenes/img-articulos/mochila.jpg"  alt="imagen de producto">
-  </div>
-  
-  <div class="descripciones">
-    <p class="nombre-txt" >Bella mochila infantil</p>
-    <p class="no-vendidos">16 Vendidos recientemente</p>
-    <p class="precio-txt">¢16.955</p>
-  </div>
-  
-  </a>`
-  
-    var numeroDeElementos = 10;
-  
-    let cantidadElementos = '';
-  
-    for (let i = 0; i < numeroDeElementos; i++) {
-      cantidadElementos += boxArticulo;
-    };
-  
-    contenedorArticulos.innerHTML = cantidadElementos;
+    articuloIndex.innerHTML = contenido;
+       return articuloIndex;
   }
   
-  insertar();
+  const cajaMatriz = document.getElementById('contenedor-articulos');
+  
+  const http = new XMLHttpRequest();
+  
+  http.open('GET', 'http://localhost:3000/articulo');
+  
+  http.send();
+  
+  http.onload = () => {
+    const data = JSON.parse(http.response);
+    data.forEach( articulo => {
+      const nuevoArticulo = crearArticuloIndex(articulo.nombreCategoria, articulo.numbVentas, articulo.nombre, articulo.precioDescuento, articulo.precioNeto, articulo.descuento, articulo.id);
+      cajaMatriz.appendChild(nuevoArticulo);
+    });
+  };
+
+
+
+
+
+
+
+
+// const insertar = () => {
+//     const contenedorArticulos = document.getElementById('contenedor-articulos');
+  
+//     contenedorArticulos.innerHTML = '';
+  
+//     const boxArticulo = `<a class="caja-articulo" href="#">
+//   <div class="caja_imagen-articulo" id="caja_imagen-articulo">
+//     <img class="imagen-articulo" id="imagen-articulo" src="src/imagenes/img-articulos/mochila.jpg"  alt="imagen de producto">
+//   </div>
+  
+//   <div class="descripciones">
+//     <p class="nombre-txt" >Bella mochila infantil</p>
+//     <p class="no-vendidos">16 Vendidos recientemente</p>
+//     <p class="precio-txt">¢16.955</p>
+//   </div>
+  
+//   </a>`
+  
+//     var numeroDeElementos = 10;
+  
+//     let cantidadElementos = '';
+  
+//     for (let i = 0; i < numeroDeElementos; i++) {
+//       cantidadElementos += boxArticulo;
+//     };
+  
+//     contenedorArticulos.innerHTML = cantidadElementos;
+//   }
+  
+//   insertar();
--- src/scripts/mostrardatos-matriz.js
+++ src/scripts/mostrardatos-matriz.js
@@ -1,3 +1,69 @@
+const crearArticuloMatriz = (nombreCategoria, numbVentas, nombre, precioDescuento, precioNeto, descuento, id) => {
+    const cajaArticulo = document.createElement('a');
+    cajaArticulo.classList.add('caja-articulo')
+    const contenido = `
+    <div class="caja__img-articulo" id="">
+      <img class="img-articulo" id="archivoImg" src="src/imagenes/img-articulos/mochila.jpg"
+        alt="imagen de producto">
+    </div>
+  
+    <div class="contenido-articulo" id="">
+  
+      <div class="caja__de-categorias">
+  
+        <div class="caja__nombre-categoria" id="">
+          <p class="nombre__de-categoria">${nombreCategoria}</p>
+        </div>
+  
+        <div class="caja__numero-ventas">
+          <p class="numero-ventas">${numbVentas + " ventas"}</p>
+        </div>
+  
+      </div>
+  
+      <h2 class="nombre__de-articulo" id="">${nombre}</h2>
+  
+      <div class="valores__de-articulo">
+  
+        <div class="caja__precio-descuento">
+          <p class="precio-descuento">${"¢" + precioDescuento}</p>
+        </div>
+  
+        <div class="caja__precio-neto">
+          <p class="precio">${"¢" + precioNeto}</p>
+        </div>
+  
+        <div class="caja__numero-descuento">
+          <p class="descuento">-${descuento}%</p>
+        </div>
+  
+      </div>
+  
+      <div class="caja__de-id">
+        <h3 class="id">${"ID:" + id }</h3>
+      </div>`
+  
+      cajaArticulo.innerHTML = contenido;
+       return cajaArticulo;
+  }
+  
+  const cajaMatriz = document.getElementById('caja__articulos-matriz');
+  
+  const http = new XMLHttpRequest();
+  
+  http.open('GET', 'http://localhost:3000/articulo');
+  
+  http.send();
+  
+  http.onload = () => {
+    const data = JSON.parse(http.response);
+    data.forEach( articulo => {
+      const nuevoArticulo = crearArticuloMatriz(articulo.nombreCategoria, articulo.numbVentas, articulo.nombre, articulo.precioDescuento, articulo.precioNeto, articulo.descuento, articulo.id);
+      cajaMatriz.appendChild(nuevoArticulo);
+    });
+  };
+
+
 // const crearArticulo = () => {
 //     event.preventDefault();
     
